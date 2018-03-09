b96eaab2ae52082e4c11ae283f792ae6d7d83443
Rutvik Mehta <rutvik1061992@gmail.com>
2018-03-09 17:46:55 +0530

    Initial commit

---
+++ app/src/main/java/in/fusionbit/karma/activity/ActivityBase.java
@@ -0,0 +1,14 @@
+package in.fusionbit.karma.activity;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rutvikmehta on 09/03/18.
+ */
+
+public class ActivityBase extends AppCompatActivity{
+
+
+
+}
---
+++ app/src/main/java/in/fusionbit/karma/activity/InitialActivity.java
@@ -0,0 +1,15 @@
+package in.fusionbit.karma.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.fusionbit.karma.R;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+    }
+}
